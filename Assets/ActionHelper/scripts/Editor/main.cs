738f8564332a8c3186eff399e99512c04700e1bc
ribeKim <ribe@ribe.io>
2021-08-10 14:34:14 +0900

    UI 변경 밑 다중 토글 기능 추가

--- Assets/ActionHelper/scripts/Editor/main.cs
+++ Assets/ActionHelper/scripts/Editor/main.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Unity.EditorCoroutines.Editor;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -8,27 +10,42 @@ namespace ActionHelper.scripts.Editor
 {
     public class Main : EditorWindow
     {
-        private const int VRC_MAX_INT = 255;
-        [SerializeField] private List<AnimationClip> animations;
+        [SerializeField] private List<Entity> entities;
         private AnimatorController _controller;
         private AnimationClip _conditionedAnimation;
         private AnimationClip _idleAnimation;
+        private Vector2 _scroll;
         private string _layerName;
         private string _parameterName;
-        private Vector2 _scroll;
         private int _tab;
+        private int _selected = Const.DEFAULT_SELECTED;
+        private GUIStyle _boxNormal;
+        private GUIStyle _boxSelected;
+
+        [MenuItem("ActionHelper/Main")]
+        private static void Init()
+        {
+            var window = (Main) GetWindow(typeof(Main));
+            window.Show();
+        }
+
+        private void OnEnable() => EditorCoroutineUtility.StartCoroutine(Update.Check(), this);
 
         private void OnGUI()
         {
-            GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-            _controller =
-                EditorGUILayout.ObjectField("Animator", _controller, typeof(AnimatorController), false) as
-                    AnimatorController;
-            _layerName = EditorGUILayout.TextField("Layer Name", _layerName);
-            _parameterName = EditorGUILayout.TextField("Parameter Name", _parameterName);
-            GUILayout.Label("Modes", EditorStyles.boldLabel);
-            _tab = GUILayout.Toolbar(_tab, new[] {"Single Toggle", "Multiple Toggle"});
+            InitStyles();
 
+            if (Update.IsUpdated)
+                GUILayout.Box(Const.MSG_UPDATE_REQUIRED, GUILayout.MaxWidth(float.MaxValue));
+
+            GUILayout.Label(Const.BASE_SETTINGS, EditorStyles.boldLabel);
+            _controller = EditorGUILayout.ObjectField(
+                Const.ANIMATOR, _controller, typeof(AnimatorController), false) as AnimatorController;
+            _layerName = EditorGUILayout.TextField(Const.LAYER_NAME, _layerName);
+            _parameterName = EditorGUILayout.TextField(Const.PARAMTER_NAME, _parameterName);
+            GUILayout.Label(Const.MODES, EditorStyles.boldLabel);
+            _tab = GUILayout.Toolbar(_tab, Const.MENUS);
+            
             switch (_tab)
             {
                 case 0:
@@ -40,39 +57,88 @@ namespace ActionHelper.scripts.Editor
             }
         }
 
-        [MenuItem("ActionHelper/Main")]
-        private static void Init()
-        {
-            var window = (Main) GetWindow(typeof(Main));
-            window.Show();
-        }
-
         private void SingleToggle()
         {
-            _idleAnimation =
-                EditorGUILayout.ObjectField("IDLE", _idleAnimation, typeof(AnimationClip), false) as AnimationClip;
-            _conditionedAnimation =
-                EditorGUILayout.ObjectField("Condition", _conditionedAnimation, typeof(AnimationClip),
-                    false) as AnimationClip;
+            _idleAnimation = EditorGUILayout.ObjectField(
+                Const.IDLE, _idleAnimation, typeof(AnimationClip), false) as AnimationClip;
+            _conditionedAnimation = EditorGUILayout.ObjectField(
+                Const.CONDITION, _conditionedAnimation, typeof(AnimationClip), false) as AnimationClip;
 
-            if (GUILayout.Button("Save")) AppendLayerWithParameter(_idleAnimation, _conditionedAnimation);
+            if (GUILayout.Button(Const.SAVE)) AppendLayerWithParameter(_idleAnimation, _conditionedAnimation);
         }
 
         private void MultipleToggle()
         {
             ScriptableObject target = this;
             var so = new SerializedObject(target);
-            var stringsProperty = so.FindProperty("animations");
+            var stringsProperty = so.FindProperty("entities");
+            EditorGUILayout.BeginHorizontal();
 
-            if (stringsProperty.arraySize > VRC_MAX_INT) stringsProperty.arraySize = VRC_MAX_INT;
+            if (GUILayout.Button(Const.ADD) && stringsProperty.arraySize < Const.VRC_MAX_INT)
+            {
+                stringsProperty.arraySize += 1;
+            }
+
+            if (GUILayout.Button(Const.REMOVE) && stringsProperty.arraySize > Const.ZERO)
+            {
+                if (_selected == Const.DEFAULT_SELECTED)
+                {
+                    stringsProperty.arraySize -= 1;
+                }
+                else
+                {
+                    stringsProperty.DeleteArrayElementAtIndex(_selected);
+                    _selected = Const.DEFAULT_SELECTED;
+                }
+            }
+
+            if (GUILayout.Button(Const.REMOVE_ALL))
+            {
+                stringsProperty.ClearArray();
+                _selected = Const.DEFAULT_SELECTED;
+            }
+
+            EditorGUILayout.EndHorizontal();
+
 
             _scroll = EditorGUILayout.BeginScrollView(_scroll, GUILayout.MaxHeight(400));
-            EditorGUILayout.PropertyField(stringsProperty, true);
+
+            for (var i = 0; i < stringsProperty.arraySize; i++)
+            {
+                DrawAnimationClips(stringsProperty, i);
+            }
+
             EditorGUILayout.EndScrollView();
+            
             so.ApplyModifiedProperties();
+            
+            if (GUILayout.Button(Const.SAVE)) AppendLayerWithParameter();
+        }
 
+        private void DrawAnimationClips(SerializedProperty sp, int index)
+        {
+            var item = sp.GetArrayElementAtIndex(index);
+            var animationClip = item.FindPropertyRelative("animationClip");
 
-            if (GUILayout.Button("Save")) AppendLayerWithParameter(animations);
+            EditorGUI.indentLevel += 1;
+            
+            var rect = EditorGUILayout.BeginHorizontal(index == _selected ? _boxSelected : _boxNormal);
+            
+            EditorGUILayout.LabelField($"{Const.CONDITION} {index}", GUILayout.MinWidth(10));
+            EditorGUILayout.PropertyField(animationClip, new GUIContent(""));
+            
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUI.indentLevel -= 1;
+
+            if (Event.current.type == EventType.MouseDown)
+            {
+                if (rect.Contains(Event.current.mousePosition))
+                {
+                    _selected = index;
+                    Event.current.Use();
+                }
+            }
         }
 
         private void AppendLayerWithParameter(Motion idleClip, Motion conditionedClip)
@@ -80,8 +146,9 @@ namespace ActionHelper.scripts.Editor
             if (CheckCondition()) return;
             AddParameterAndLayer(AnimatorControllerParameterType.Bool);
 
-            var firstState = _controller.layers.Last().stateMachine.AddState("IDLE", new Vector3(400, 0, 0));
-            var lastState = _controller.layers.Last().stateMachine.AddState("Condition", new Vector3(400, 100, 0));
+            var stateMachine = StateMachinePosition();
+            var firstState = stateMachine.AddState(Const.IDLE, new Vector3(400, 0));
+            var lastState = stateMachine.AddState(Const.CONDITION, new Vector3(400, 100));
 
             firstState.AddTransition(lastState)
                 .AddCondition(AnimatorConditionMode.If, 0, _parameterName);
@@ -92,20 +159,19 @@ namespace ActionHelper.scripts.Editor
             lastState.motion = conditionedClip;
         }
 
-        private void AppendLayerWithParameter(IList<AnimationClip> clips)
+        private void AppendLayerWithParameter()
         {
             if (CheckCondition()) return;
             AddParameterAndLayer(AnimatorControllerParameterType.Int);
-
-            foreach (var animationClip in clips)
+            
+            for (var i = 0; i < entities.Count; i++)
             {
-                var index = clips.IndexOf(animationClip);
-                var stateMachine = _controller.layers.Last().stateMachine;
+                var stateMachine = StateMachinePosition();
                 var state = _controller.layers.Last().stateMachine
-                    .AddState(animationClip.name, new Vector3(400, index * 100, 0));
+                    .AddState(entities[i].animationClip.name, new Vector3(400, i * 100));
                 stateMachine.AddAnyStateTransition(state)
-                    .AddCondition(AnimatorConditionMode.Equals, index, _parameterName);
-                state.motion = animationClip;
+                    .AddCondition(AnimatorConditionMode.Equals, i, _parameterName);
+                state.motion = entities[i].animationClip;
             }
         }
 
@@ -113,18 +179,18 @@ namespace ActionHelper.scripts.Editor
         {
             if (!_controller)
             {
-                Alert("Requirement: Controller");
+                Alert(Const.MSG_REQUIREMENTS);
                 return true;
             }
 
             if (string.IsNullOrEmpty(_parameterName) || string.IsNullOrEmpty(_layerName))
             {
-                Alert("Enter ParameterName or LayerName");
+                Alert(Const.MSG_ENTER_PARAMETER_NAME_OR_LAYER_NAME);
                 return true;
             }
 
             if (_controller.parameters.All(x => x.name != _parameterName)) return false;
-            Alert("Duplicated ParameterName");
+            Alert(Const.MSG_DUPLICATED);
             return true;
         }
 
@@ -132,6 +198,7 @@ namespace ActionHelper.scripts.Editor
         {
             _controller.AddParameter(_parameterName, type);
             _controller.AddLayer(_layerName);
+
             var layers = _controller.layers;
             layers.Last().defaultWeight = 1;
             _controller.layers = layers;
@@ -139,7 +206,38 @@ namespace ActionHelper.scripts.Editor
 
         private void Alert(string content)
         {
-            EditorUtility.DisplayDialog("Alert", content, "Ok");
+            EditorUtility.DisplayDialog(Const.ALERT, content, Const.OK);
+        }
+
+        private void InitStyles()
+        {
+            //Normal
+            if (_boxNormal == null) _boxNormal = new GUIStyle(GUI.skin.box);
+
+            //Selected
+            if (_boxSelected != null) return;
+            _boxSelected = new GUIStyle(GUI.skin.box)
+            {
+                normal = {background = MakeStyleBackground(new Color(0.0f, 0.5f, 1f, 0.5f))}
+            };
+        }
+
+        private AnimatorStateMachine StateMachinePosition()
+        {
+            var stateMachine = _controller.layers.Last().stateMachine;
+            stateMachine.entryPosition = new Vector3(0, 0);
+            stateMachine.anyStatePosition = new Vector3(0, 200);
+            stateMachine.exitPosition = new Vector3(0, 400);
+
+            return stateMachine;
+        }
+
+        private Texture2D MakeStyleBackground(Color color)
+        {
+            var texture = new Texture2D(1, 1);
+            texture.SetPixel(0, 0, color);
+            texture.Apply();
+            return texture;
         }
     }
 }
