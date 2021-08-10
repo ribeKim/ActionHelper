aa8570911065182223173b70bf1bf347e49b7853
ribeKim <ribe@ribe.io>
2021-08-10 14:35:18 +0900

    Const 추가

---
+++ Assets/ActionHelper/scripts/Editor/const.cs
@@ -0,0 +1,28 @@
+﻿namespace ActionHelper.scripts.Editor
+{
+    public class Const
+    {
+        public static readonly string[] MENUS = { "Single Toggle", "Multiple Toggle" };
+        public const string MSG_REQUIREMENTS = "Requirement: Controller";
+        public const string MSG_ENTER_PARAMETER_NAME_OR_LAYER_NAME = "Enter ParameterName or LayerName";
+        public const string MSG_DUPLICATED = "Duplicated ParameterName";
+        public const string MSG_UPDATE_REQUIRED = "Please Update ActionHelper";
+        public const string BASE_SETTINGS = "Base Settings";
+        public const string ANIMATOR = "Animator";
+        public const string LAYER_NAME = "Layer Name";
+        public const string PARAMTER_NAME = "Parameter Name";
+        public const string MODES = "Modes";
+        public const string IDLE = "IDLE";
+        public const string CONDITION = "Condition";
+        public const string SAVE = "Save";
+        public const string ALERT = "Alert";
+        public const string OK = "Ok";
+        public const string ADD = "+";
+        public const string REMOVE = "-";
+        public const string REMOVE_ALL = "Clear";
+        public const float UPDATE_TIMER = 5 * 60;
+        public const int VRC_MAX_INT = 255;
+        public const int DEFAULT_SELECTED = -1;
+        public const int ZERO = 0;
+    }
+}
