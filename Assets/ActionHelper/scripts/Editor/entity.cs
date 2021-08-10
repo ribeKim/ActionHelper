a34ac5ceebd84fbe59dadb50bb0694bb73d26dc4
ribeKim <ribe@ribe.io>
2021-08-10 14:35:06 +0900

    다중 토글에서 사용하는 오브젝트 이동

---
+++ Assets/ActionHelper/scripts/Editor/entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace ActionHelper.scripts.Editor
+{
+    [Serializable]
+    public class Entity
+    {
+        /// <summary>
+        /// 이름
+        /// </summary>
+        public string name;
+        
+        /// <summary>
+        /// 애니메이션 클립
+        /// </summary>
+        public AnimationClip animationClip;
+    }
+}
